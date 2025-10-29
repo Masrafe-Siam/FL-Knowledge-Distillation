7ddb9d5f917b97748fcc054e3afdccd1b998fdeb
Rayhan <rayhankhan1266@gmail.com>
2025-10-30 03:56:55 +0600

    implemented mobilenetv3 and add in the model engine

--- models/modelEngine.py
+++ models/modelEngine.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 
 from models.HybridViTCNNMLP import HybridViTCNNMLP
 from models.cnn import CustomCNN
+from models.mobilenetv3 import MobileNetV3
 
 
 def get_model(model_name: str, num_classes: int, pretrained: bool = True, dropout_rate: float = 0.5):
@@ -16,6 +17,12 @@ def get_model(model_name: str, num_classes: int, pretrained: bool = True, dropou
         )
     elif model_name == 'cnn':
         model = CustomCNN(num_classes=num_classes)
+    elif model_name == 'mobilenetv3':
+        model = MobileNetV3(
+            num_classes=num_classes,
+            pretrained=pretrained,
+            dropout_rate=dropout_rate
+        )
 
     else:
         raise ValueError(f"Model '{model_name}' is not supported.")
